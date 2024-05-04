290d06fd331e5af2bf03c012848295833fd267e9
BINDE <nannimbinde@gmail.com>
2024-05-04 02:59:22 +0100

    implementation of spring security to banking application

---
+++ src/main/java/com/binde/bankapplication/entity/Role.java
@@ -0,0 +1,6 @@
+package com.binde.bankapplication.entity;
+
+public enum Role {
+    ROLE_USER,
+    ROLE_ADMIN
+}
---
+++ src/main/java/com/binde/bankapplication/repository/UserRepository.java
@@ -0,0 +1,20 @@
+package com.binde.bankapplication.repository;
+
+
+import com.binde.bankapplication.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+    Boolean existsByEmail(String email);
+
+   // Optional<User> findByEmail(String emaIL);
+    Optional<User> findByEmail(String email);
+
+    boolean existsByAccountNumber(String accountNumber);
+
+    User findByAccountNumber(String accountNumber);
+
+
+}
